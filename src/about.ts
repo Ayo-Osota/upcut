11ff8a28f11939fb6e12be80be6a988e3960475e
Ayo-Osota <osotaayomikun@gmail.com>
2024-06-24 00:03:50 +0100

    Remove imports of typescriptLogo and viteLogo from about.ts

--- src/about.ts
+++ src/about.ts
@@ -1,6 +1,4 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { navbar } from './components/navbar.ts';
 import { navigationLinks } from './data.ts';
 import { footer } from './components/footer.ts';
